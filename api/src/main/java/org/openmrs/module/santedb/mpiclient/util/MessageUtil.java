fb1c9c41358413a9b15c4f635684cb85a13cee0c
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-11-30 17:02:33 +0630

    Updated readme

--- api/src/main/java/org/openmrs/module/santedb/mpiclient/util/MessageUtil.java
+++ api/src/main/java/org/openmrs/module/santedb/mpiclient/util/MessageUtil.java
@@ -1,3 +1,19 @@
+/**
+ * Original Copyright (c) 2014-2018 Justin Fyfe (Fyfe Software Inc.) 
+ * Copyright (c) 2018 SanteDB Community
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ */
 package org.openmrs.module.santedb.mpiclient.util;
 
 import java.io.IOException;
@@ -799,16 +815,17 @@ public final class MessageUtil {
 		PersonName pn = new PersonName();
 		
 		if(xpn.getFamilyName().getSurname().getValue() == null || xpn.getFamilyName().getSurname().getValue().isEmpty())
-			pn.setFamilyName("");
+			pn.setFamilyName("(NULL)");
 		else
 			pn.setFamilyName(xpn.getFamilyName().getSurname().getValue());
 		pn.setFamilyName2(xpn.getFamilyName().getSurnameFromPartnerSpouse().getValue());
 		
 		// Given name
 		if(xpn.getGivenName().getValue() == null || xpn.getGivenName().getValue().isEmpty())
-			pn.setGivenName("");
+			pn.setGivenName("(NULL)");
 		else
 			pn.setGivenName(xpn.getGivenName().getValue());
+		
 		pn.setMiddleName(xpn.getSecondAndFurtherGivenNamesOrInitialsThereof().getValue());
 		pn.setPrefix(xpn.getPrefixEgDR().getValue());
 		
