1c366aa055b48aadfb100a9b3752a3d400a946b8
Justin Fyfe <justin@fyfesoftware.ca>
2020-02-26 05:19:24 -0500

    Updated OMOD on 1.9.x to re-add the link for MPI search

--- api/src/main/java/org/openmrs/module/santedb/mpiclient/api/MpiClientService.java
+++ api/src/main/java/org/openmrs/module/santedb/mpiclient/api/MpiClientService.java
@@ -1,6 +1,7 @@
 /**
- * Copyright (c) 2014-2020 Fyfe Software Inc. 
- * 
+ * Portions Copyright 2015-2018 Mohawk College of Applied Arts and Technology
+ * Portions Copyright (c) 2014-2020 Fyfe Software Inc.
+ *  
  * Licensed under the Apache License, Version 2.0 (the "License"); you 
  * may not use this file except in compliance with the License. You may 
  * obtain a copy of the License at 
