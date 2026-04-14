e13a864bb80b272e19da93294b28bb65bf5f2a9e
mherman22 <hermanmuhereza22@gmail.com>
2026-04-14 15:14:48 +0000

    Apply same cleanup hardening to PatientSyncWorker

--- api/src/main/java/org/openmrs/module/santedb/mpiclient/aop/PatientSyncWorker.java
+++ api/src/main/java/org/openmrs/module/santedb/mpiclient/aop/PatientSyncWorker.java
@@ -75,11 +75,15 @@ public class PatientSyncWorker extends Thread {
 			
 			Context.openSession();
 			Context.setUserContext(this.m_userContext);
-			Context.addProxyPrivilege("Get Identifier Types");
-			Context.addProxyPrivilege("Get Patients");
-			Context.addProxyPrivilege("Get Patient Identifiers");
-			Context.addProxyPrivilege("Edit Patient Identifiers");
-			Context.addProxyPrivilege("Add Patient Identifiers");
+			try {
+				Context.addProxyPrivilege("Get Identifier Types");
+				Context.addProxyPrivilege("Get Patients");
+				Context.addProxyPrivilege("Get Patient Identifiers");
+				Context.addProxyPrivilege("Edit Patient Identifiers");
+				Context.addProxyPrivilege("Add Patient Identifiers");
+			} catch (Exception e) {
+				log.debug("Error adding proxy privileges", e);
+			}
 			MpiClientService hieService = Context.getService(MpiClientService.class);
 			// Grab the national health ID for the patient
 			if(this.m_configuration.getAutomaticCrossReferenceDomains() != null) {
@@ -135,15 +139,23 @@ public class PatientSyncWorker extends Thread {
 			log.error(e);
 		}		
 		finally {
-			Context.removeProxyPrivilege("Get Identifier Types");
-			Context.removeProxyPrivilege("Get Patients");
-			Context.removeProxyPrivilege("Get Patient Identifiers");
-			Context.removeProxyPrivilege("Edit Patient Identifiers");
-			Context.removeProxyPrivilege("Add Patient Identifiers");
 			synchronized (s_xref) {
 				s_xref.remove(this.m_patient.getUuid());
 			}
-			Context.closeSession();
+			try {
+				Context.removeProxyPrivilege("Get Identifier Types");
+				Context.removeProxyPrivilege("Get Patients");
+				Context.removeProxyPrivilege("Get Patient Identifiers");
+				Context.removeProxyPrivilege("Edit Patient Identifiers");
+				Context.removeProxyPrivilege("Add Patient Identifiers");
+			} catch (Exception e) {
+				log.debug("Error removing proxy privileges", e);
+			}
+			try {
+				Context.closeSession();
+			} catch (Exception e) {
+				log.debug("Error closing session", e);
+			}
 		}
 	}
 }
